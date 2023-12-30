4ce650d74023d754b2f0e9245ece5987081d4adb
sagar-wandile <wandilesagar2926@gmail.com>
2023-12-30 08:29:48 +0530

    final changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import copyIcon from './icons8-copy-24.png'
 import showToast from 'crunchy-toast';
 import axios from 'axios'
+import { set } from 'mongoose';
 function App() {
 
   const [url, setUrl] = useState('')
   const [slug, setSlug] = useState('')
   const [shortUrl, setShortUrl] = useState('')
+  const [links, setLinks] = useState([]);
 
   const  genratelink = async () => {
     const response = await axios.post('/link' ,{
@@ -21,7 +23,15 @@ function App() {
     showToast('copy to clickboard', 'success', 2000);
   }
 
+  const loadlinks = async () => {
+    const response = await axios.get('/api/links');
+    setLinks(response?.data?.data);
+  }
 
+  useEffect(() =>{
+    loadlinks();
+  } , [links])
+   
   return (
     <div>
       <h1 className='main-container'>Quick Links</h1>
@@ -63,8 +73,33 @@ function App() {
             >Do Magik 🪄</button>
 
         </div>
-        <div>
-          <h2>All Links</h2>
+        <div className='all-links-container'>
+          
+          {
+            links.map((linkojb, index)=>{
+              const {url, slug, clicks} = linkojb;
+              return(
+                <div className='all-links-card'>
+                <p className='card-url'> <b>URL :</b> {url}</p>
+                <p className='card-slug'><b>Short URL : </b>{process.env.REACT_APP_BASE_URL}/{slug}</p>
+                <p className='card-clicks'>Clicks :{clicks}</p>
+                </div>
+              )
+            })
+          }
+          {/* {
+            links?.map((linkObj, index)=>{
+              const {url, slug, clicks } = linkObj;
+
+              return(
+                <div>
+                 <p> URL: {url}</p>
+                 <p> ShortUrl : {slug}</p>
+                 <p> Clicks : {clicks}</p>
+                </div>
+              )
+            })
+          } */}
         </div>
       </div>
 
